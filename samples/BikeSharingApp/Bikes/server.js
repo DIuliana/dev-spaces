9b3442aace6891bd86c45ae36f2a8fa5958b7170
DIuliana <danea.iuliana@gmail.com>
2019-11-12 13:51:09 +0200

    no

--- samples/BikeSharingApp/Bikes/server.js
+++ samples/BikeSharingApp/Bikes/server.js
@@ -230,6 +230,7 @@ app.get('/api/bikes/:bikeId', function(req, res) {
 
         var theBike = result;
         // Hard code image url *FIX ME*
+		//no 
         theBike.imageUrl = "/static/logo.svg";
         theBike.id = theBike._id;
         delete theBike._id;
